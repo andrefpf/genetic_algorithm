167997eb8bac3e931ab8cac414d5cb4c567270b5
André Filipe da Silva Fernandes <fpf.andre@gmail.com>
2019-09-17 17:32:39 -0300

    showing graph and evolution at the same time, but not properly

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from environment import Environment
 def math_function(x, y=0):
     # return 100 * np.sqrt(np.abs(y - (0.01 * x**2))) + 0.01*np.abs(x + 10)
     return -(y + 47) * np.sin(np.sqrt(np.abs(y + x/2 + 47))) - x*np.sin(np.sqrt(np.abs(x-(y+47))))
+    # return x**2 + y**2
 
 def plot_function():
     scale = 10
@@ -16,17 +17,13 @@ def plot_function():
     x, y = np.meshgrid(x, y)
     z = math_function(x, y)
 
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, antialiased=False)
+    fig = plt.figure(figsize=plt.figaspect(0.5))
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    ax.plot_surface(x, y, z, cmap=cm.coolwarm, antialiased=False)
+
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    ax.scatter(env.tragetory_x, env.tragetory_y, env.tragetory_score)
     plt.show()
-
-    # bx = fig.add_subplot(111, projection='3d')
-    # bx.scatter(env.tragetory_x, env.tragetory_y, env.tragetory_score)
-    # plt.show()
-
-
-
 
 start = time() 
 env = Environment(math_function, x_range=(-512, 512), y_range=(-512, 512))
