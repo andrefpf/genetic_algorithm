0ee8fb1078f674906495d1535fc592bfb610748f
André Filipe da Silva Fernandes <fpf.andre@gmail.com>
2019-09-27 14:01:40 -0300

    checking procedure time

--- ndfunctions/main.py
+++ ndfunctions/main.py
@@ -1,11 +1,16 @@
 import numpy as np
 from environment import Environment
+from time import time 
 
 def math_function(var):
     return var['x']
+
+start = time()
 
 var= ['x']
 interval = [(0,2)]
 
 env = Environment(math_function, var, interval)
 env.start()
+
+print(time() - start)
